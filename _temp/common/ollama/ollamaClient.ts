2dc2cd75c36aaf1bf76fee97da10f8c825890f02
Dmytro Novikov <nodm.pe@gmail.com>
2025-01-09 21:29:33 +0200

    Remove unused Ollama integration files and related prompt generation code

--- _temp/common/ollama/ollamaClient.ts
+++
@@ -1,9 +0,0 @@
-import {Ollama} from 'ollama';
-import {getEnvVariable} from '../../../src/common/getEnvVariable';
-
-const host = getEnvVariable('OLLAMA_API_URL');
-
-export const ollamaModel = getEnvVariable('OLLAMA_MODEL');
-export const ollama = new Ollama({
-  host,
-});
--- _temp/steps/index.ts
+++
@@ -1,1 +0,0 @@
-export {generateCodeReview} from './generateCodeReview';
